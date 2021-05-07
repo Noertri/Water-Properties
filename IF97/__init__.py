f8eb943654f2d13bda96dd97d3d608744cca53c1
Tri Nur Syaifudin <trinuruns@gmail.com>
2021-05-07 19:46:04 +0700

    Add files via upload

---
+++ IF97/__init__.py
@@ -0,0 +1,2 @@
+from .koefisien import bigr, rhoc, tempc, pressc, presst, tempt
+from ._if97_ import water97
---
+++ IF97/koefisien.py
@@ -0,0 +1,15 @@
+from ._IJnReg1_ import koefReg1
+from ._IJnReg2_ import koefReg2
+from ._IJnReg3_ import koefReg3
+from ._IJnReg4_ import koefReg4
+from ._IJnReg5_ import koefReg5
+from ._IJnPTReg3_ import koefSubReg3
+from ._IJnBound_ import koefB3, b23
+
+#Konstanta umum
+tempc = 647.096
+pressc = 22064.
+rhoc = 322.
+bigr = 0.461526
+tempt = 273.16
+presst = 0.611657
